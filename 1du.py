ab94f3a3ccbeb6583681916340ea5995ef3170c5
Iryna Polko <polko-irina@seznam.cz>
2022-09-25 22:58:33 +0200

    ukol-1

---
+++ 1du.py
@@ -0,0 +1,37 @@
+baliky = {
+    "B541X": True,
+    "B547X": False,
+    "B251X": False,
+    "B501X": True,
+    "B947X": False,
+}
+
+i = input(" Zadej kód balíku : ")
+if baliky["B541X"] == True :
+    print("Balík byl předán kurýrovi")
+else :
+    print("Balík zatím nebyl předán kurýrovi")
+
+a = input(" Zadej kód balíku : ")
+if baliky["B547X"] == True :
+    print("Balík byl předán kurýrovi")
+else :
+    print("Balík zatím nebyl předán kurýrovi")
+
+b = input(" Zadej kód balíku : ")
+if baliky["B251X"] == True :
+    print("Balík byl předán kurýrovi")
+else :
+    print("Balík zatím nebyl předán kurýrovi")
+
+c = input(" Zadej kód balíku : ")
+if baliky["B501X"] == True :
+    print("Balík byl předán kurýrovi")
+else :
+    print("Balík zatím nebyl předán kurýrovi")
+
+d = input(" Zadej kód balíku : ")
+if baliky["B947X"] == True :
+    print("Balík byl předán kurýrovi")
+else :
+    print("Balík zatím nebyl předán kurýrovi")
