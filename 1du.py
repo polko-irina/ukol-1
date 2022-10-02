ef52b7d294a84b88cea0f56b82a1a4ed835b9ae4
Iryna Polko <polko-irina@seznam.cz>
2022-10-02 08:34:55 +0200

    DU

--- 1du.py
+++ 1du.py
@@ -6,4 +6,12 @@ baliky = {
     "B947X": False,
 }
 
+balik = input(" Zadej kód balíku : ")
 
+if balik in baliky:
+    if baliky[balik]:
+        print("Balík byl doručen")
+    else :
+        print ("Balík zatím nebyl doručen")
+else:
+    print (" Balik neexistuje ")
